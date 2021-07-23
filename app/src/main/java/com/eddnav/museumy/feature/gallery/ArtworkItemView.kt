53a59859a9371ee976e727c82eebef912f10c50a
Eduardo Naveda <eddnav@gmail.com>
2021-07-23 21:45:32 +0200

    Add ArtworkItemView base, enable ViewBinding

---
+++ app/src/main/java/com/eddnav/museumy/feature/gallery/ArtworkItemView.kt
@@ -0,0 +1,31 @@
+package com.eddnav.museumy.feature.gallery
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.eddnav.museumy.databinding.ViewArtworkItemBinding
+
+class ArtworkItemView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val binding = ViewArtworkItemBinding.inflate(LayoutInflater.from(context))
+
+    var artworkItem: ArtworkItem? = null
+        set(value) {
+            field = value
+            update()
+        }
+
+    init {
+        addView(binding.root)
+    }
+
+    private fun update() {
+        val artworkItem = artworkItem ?: return
+        binding.title.text = artworkItem.artwork.title
+    }
+}
